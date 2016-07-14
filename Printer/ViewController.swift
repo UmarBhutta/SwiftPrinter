7be6ceae2ff22d60192b459e94f61042c9ed9707
UmarBhutta <malik.umar@msn.com>
2016-07-14 10:33:16 +0400

    Initial Commit

---
+++ Printer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Printer
+//
+//  Created by Malik Umar Bhutta on 7/14/16.
+//  Copyright © 2016 Malik Umar Bhutta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
